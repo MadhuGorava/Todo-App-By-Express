3f936fe2e3df022bc0db70256d789b9a51d90235
MadhuGorava <goravam143@gmail.com>
2022-09-25 16:14:43 +0000

    Todo App By Express

--- app.js
+++ app.js
@@ -33,15 +33,79 @@ const convertPlayerToResponseObject = (objectItem) => {
   return {};
 };
 
-Express.get("/todos/", (request, response) => {
-  const todoQuery = `select * from todo`;
-  const responseQuery = db.all(todoQuery);
+Express.get("/todos/", async (request, response) => {
+  const { status } = request.query;
+  const todoQuery = `select * from todo where status = ${status}`;
+  const responseQuery = await db.all(todoQuery);
   response.send(responseQuery);
   console.log(todo);
 });
 
-Express.get("/todos/", (request, response) => {
-  const todoQuery = `select * from todo`;
-  const responseQuery = db.all(todoQuery);
+Express.get("/todos/", async (request, response) => {
+  const { priority } = request.query;
+  const todoQuery = `select * from todo where priority = ${priority}`;
+  const responseQuery = await db.all(todoQuery);
   response.send(responseQuery);
 });
+
+Express.get("/todos/", async (request, response) => {
+  const { priority, status } = request.query;
+  const todoQuery = `select * from todo where priority = ${priority} and status = ${status}`;
+  const responseQuery = await db.all(todoQuery);
+  response.send(responseQuery);
+});
+
+Express.get("/todos/", async (request, response) => {
+  const { searchQ } = request.query;
+  const todoQuery = `select * from todo where search_q like "%${searchQ}%"`;
+  const responseQuery = await db.all(todoQuery);
+  response.send(responseQuery);
+});
+
+Express.get("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const todoQuery = `select * from todo where id = ${todoId}`;
+  const responseQuery = await db.all(todoQuery);
+  response.send(responseQuery);
+});
+
+Express.post("/todos/", async (request, response) => {
+  const { todoId, todo } = request.body;
+  const { priority, status } = request.query;
+  const todoQuery = `insert into todo (id, todo, priority, status) values (${todoId}, '${todo}', '${priority}', '${status}')`;
+  await db.all(todoQuery);
+  response.send("Todo Successfully Added");
+});
+
+Express.put("/todos/:todoId", async (request, response) => {
+  const { todoId } = request.params;
+  const { status } = request.query;
+  const todoQuery = `update todo set status = '${status}' where todo_id = ${todoId}`;
+  await db.all(todoQuery);
+  response.send("Status Updated");
+});
+
+Express.put("/todos/:todoId", async (request, response) => {
+  const { todoId } = request.params;
+  const { priority } = request.query;
+  const todoQuery = `update todo set priority = '${priority}' where todo_id = ${todoId}`;
+  await db.all(todoQuery);
+  response.send("Priority Updated");
+});
+
+Express.put("/todos/:todoId", async (request, response) => {
+  const { todoId } = request.params;
+  const { todo } = request.body;
+  const todoQuery = `update todo set todo = '${todo}' where todo_id = ${todoId}`;
+  await db.all(todoQuery);
+  response.send("Todo Updated");
+});
+
+Express.delete("/todos/:todoId", async (request, response) => {
+  const { todoId } = request.params;
+  const todoQuery = `delete todo where todo_id = ${todoId}`;
+  await db.all(todoQuery);
+  response.send("Todo Deleted");
+});
+
+module.exports = Express;
